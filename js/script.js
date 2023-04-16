aa52ad33c33551b302daa3692bb19a08973cd74b
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-04-17 00:16:42 +0800

    edited typo

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ var todoListContainer = document.getElementById("todo-list");
 
 //Step 2 Write the behaviour
 function onAddTaskClicked(event) {
-    var taskname = newTaskInput.valuex;
+    var taskname = newTaskInput.value;
     newTaskInput.value ="";
-    todoListContainer.insertAdjacentHTML('afterbegin', taskName);
+    todoListContainer.insertAdjacentHTML('afterbegin', taskname);
 }
 
 // Step 3 link to event handler
