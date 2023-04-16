ec2117ed7a46b0118cb1f41502b9f96645758a13
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-04-17 00:25:09 +0800

    added syntax

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var addTaskButton = document.getElementById("add-task");
 var newTaskInput = document.getElementById("task-input");
 var todoListContainer = document.getElementById("todo-list");
-var templateContainer = document.getElementById("list-item-template")
+var templateContainer = document.getElementById("list-item-template");
 var template = templateContainer.innerHTML
 
 
